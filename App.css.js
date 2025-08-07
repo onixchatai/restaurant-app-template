9a7fb33c8410844047749670bedd471a5b9405c0
Vikstagram <onixchatai@gmail.com>
2025-08-07 15:40:56 -0700

    Add files via upload

---
+++ App.css.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CustomerMobileApp from './components/CustomerMobileApp';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomerMobileApp />
+    </div>
+  );
+}
+
+export default App;
